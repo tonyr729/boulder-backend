2db13c802ca17675249b12d6c6e30c7a2f078071
Tony Robertson <tonyr729@gmail.com>
2018-09-09 11:58:11 -0600

    Set up server code and basic endpoint to provide all data

---
+++ server.js
@@ -0,0 +1,31 @@
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const bodyParser = require('body-parser');
+
+app.set('port', process.env.PORT || 3001);
+app.use(bodyParser.json());
+
+
+app.get('/api/v1/all/', (request, response) => {
+  return database('weather').select()
+    .then(weather => {
+      return response.status(200).json({
+        status: 'success',
+        weather
+      });
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
+});
+
+
+server.listen(app.get('port'), () => {
+  console.log(`Express intro running on localhost: ${app.get('port')}`);
+});
+
+module.exports = app;
