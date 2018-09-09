2dfc62dc8eb75b49bdc9096fc82fd0139f4efe40
Tony Robertson <tonyr729@gmail.com>
2018-09-09 11:58:56 -0600

    Set up endpoint to provide data from specific year

--- server.js
+++ server.js
@@ -23,6 +23,23 @@ app.get('/api/v1/all/', (request, response) => {
     });
 });
 
+app.get('/api/v1/year/:yearID', (request, response) => {
+  const yearID = request.params.yearID;
+  return database('weather').where({
+    year: yearID
+  }).select()
+    .then(year => {
+      return response.status(200).json({
+        status: 'success',
+        year
+      });
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
+});
+
+
 
 server.listen(app.get('port'), () => {
   console.log(`Express intro running on localhost: ${app.get('port')}`);
